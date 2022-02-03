cbcd2008ff2f7fee4ba3addf8e0f149d5c9422fb
Irtiza Hafiz <irtiza@irtiza.local>
2022-02-02 18:35:55 -0800

    working upload frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Center,
+  Text,
+  Heading,
+  VStack,
+  Button,
+  Input,
+  HStack,
+  Container,
+} from "@chakra-ui/react";
+
+import { useState, useEffect } from "react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
+  const [isSelected, setIsSelected] = useState(false);
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const onInputChange = (e) => {
+    console.log(e.target.files[0]);
+    setIsSelected(true);
+    setSelectedFile(e.target.files[0]);
+  };
+
+  const onFileUpload = (e) => {
+    console.log("About to upload file! Yay!");
+    const requestBody = JSON.stringify({
+      fileName: selectedFile.name,
+      fileToUpload: selectedFile,
+    });
+    console.log(requestBody);
+    //TODO: Make the API call to upload
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Center bg="black" color="white" padding={8}>
+        <VStack spacing={7}>
+          <Heading>Your Gallery</Heading>
+          <Text>Take a look at all your photos!</Text>
+          <HStack>
+            <input type="file" onChange={onInputChange}></input>
+
+            <Button
+              size="lg"
+              colorScheme="red"
+              isDisabled={!isSelected}
+              onClick={onFileUpload}
+            >
+              Upload Photo
+            </Button>
+          </HStack>
+        </VStack>
+      </Center>
+    </ChakraProvider>
   );
 }
 
